9ac26783e9391c02bed3c9cf880d5e34a0d66f97
jeremy <jeremydaniel.bouvier@gmail.com>
2017-01-30 16:40:47 -0500

    Add Async and Msil

---
+++ CSharpTests/Async/AsyncTest.cs
@@ -0,0 +1,151 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CSharpTests.Async
+{
+    public sealed class SingleThreadSynchronizationContext : SynchronizationContext
+    {
+
+        public List<KeyValuePair<SendOrPostCallback, object>> CallBacks =
+            new List<KeyValuePair<SendOrPostCallback, object>>();
+
+        public override void Post(SendOrPostCallback d, object state)
+        {
+            if (d == null) throw new ArgumentNullException(nameof(d));
+            Console.WriteLine("A delegate/state pair was added to the List of callBacks");
+            CallBacks.Add(new KeyValuePair<SendOrPostCallback, object>(d, state));
+        }
+
+        public override void Send(SendOrPostCallback d, object state)
+        {
+            throw new NotSupportedException("Synchronously sending is not supported.");
+        }
+
+    }
+    /// <summary>
+    /// We need to improve this test after learning lock and concurent collections
+    /// </summary>
+    [TestClass]
+    public class AsyncTest
+    {
+        public TestContext TestContext { get; set; }
+
+        public List<Task> IoTasks = new List<Task>();
+
+        public async Task<string> LoginLibraryAsync(int k)
+        {
+            //Simulate IO operation 
+            var ioTask = Task.Run(() =>
+            {
+                #region IO Thread
+
+                TestContext.WriteLine($" An Io operation is starting on thread : {Thread.CurrentThread.ManagedThreadId}");
+
+                for (var j = 0; j < 100000; j++)
+                {
+                    TestContext.WriteLine("-");
+                }
+                //throw new Exception("Exception inside the third party library...Aie Aie Aie");
+                return "Successfully logged";
+
+                #endregion
+            });
+
+
+            IoTasks.Add(ioTask);
+
+            try
+            {
+                var methodResult = await ioTask;
+
+                //The state machines executes THIS CHUNK OF CODE ON THE MAIN THREAD (THE FAKE UI THREAD)
+                TestContext.WriteLine(
+                    $"The LoginLibraryAsync Callback is now executed from : {Thread.CurrentThread.ManagedThreadId} the value of k was stored in state : {k}"
+                );
+                //throw new Exception("Exception inside our callback...Aie Aie Aie");
+
+                //Warning the returned task is not ioTask!!
+                return methodResult;
+            }
+            catch (Exception)
+            {
+                throw new Exception("The task is faulted");
+            }
+        }
+
+        [TestMethod]
+        public void Assert_Async()
+        {
+            //HERE WE ARE IN THE MAIN MESSAGE LOOP
+
+            var syncCtx = new SingleThreadSynchronizationContext();
+            SynchronizationContext.SetSynchronizationContext(syncCtx);
+
+            TestContext.WriteLine($"Main Thread {Thread.CurrentThread.ManagedThreadId}");
+
+            for (var k = 0; k < 1; k++)
+            {
+                TestContext.WriteLine($"Call the async Method");
+                var loginTask = LoginLibraryAsync(k);
+
+                //DEAD LOCK 
+                //Because the async operation needs to finish in the main thread
+                //(But the main thread is not free he's waiting the completion of the Task)
+                //LoginLibraryAsync().Wait();
+
+                var ioTaskCompleted = false;
+
+                TestContext.WriteLine("WORKING ON THE MAIN THREAD");
+                for (var i = 0; i < 200000; i++)
+                {
+                    TestContext.WriteLine(".");
+
+                    if (syncCtx.CallBacks.Count == 1 && !ioTaskCompleted)
+                    {
+                        ioTaskCompleted = true;
+                        foreach (var t in IoTasks)
+                        {
+                            TestContext.WriteLine(
+                                $"task id : {t.Id}  iscompleted : {t.IsCompleted} isFaulted : {t.IsFaulted} ");
+                        }
+                    }
+                }
+
+                TestContext.WriteLine($" LOGINTASK task id : {loginTask.Id}  iscompleted : {loginTask.IsCompleted} isFaulted : {loginTask.IsFaulted}");
+                Assert.AreEqual(loginTask.IsCompleted,false);
+                foreach (var workItem in syncCtx.CallBacks)
+                {
+                    var storedDelegate = workItem.Key;
+                    var state = workItem.Value;
+                    storedDelegate(state);
+                }
+                TestContext.WriteLine($" LOGINTASK task id : {loginTask.Id}  iscompleted : {loginTask.IsCompleted} isFaulted : {loginTask.IsFaulted}");
+                Assert.AreEqual(loginTask.IsCompleted, true);
+                try
+                {
+                    TestContext.WriteLine(loginTask.Result);
+                }
+                catch (Exception ex)
+                {
+
+                    TestContext.WriteLine(ex.Message);
+                }
+
+                /*Cleanup ioTasks*/
+                IoTasks = new List<Task>();
+                /*Cleanup callBacks*/
+                syncCtx.CallBacks = new List<KeyValuePair<SendOrPostCallback, object>>();
+
+                TestContext.WriteLine($"The Main Loop has runned {k + 1} times");
+            }
+
+        }
+
+    } 
+
+}
--- CSharpTests/MSIL/MsilTest.cs
+++ CSharpTests/MSIL/MsilTest.cs
@@ -178,7 +178,7 @@ namespace CSharpTests.MSIL
             il.Emit(OpCodes.Ret);
 
             var d = (Func<int, int>)dynamicSomeClassCalculateMethod.CreateDelegate(typeof(Func<int, int>));
-            Assert.AreEqual(d(1000),3000);
+            Assert.AreEqual(d(1000), 3000);
         }
 
         public static string Print(string s)
@@ -199,8 +199,191 @@ namespace CSharpTests.MSIL
             il.Emit(OpCodes.Ldarg_0);
             il.Emit(OpCodes.Call, methodInfo);
             il.Emit(OpCodes.Ret);
-            var callPrintMethodDelegate = (Func<string,string>)dynamicCallPrintMethod.CreateDelegate(typeof(Func<string,string>));
+            var callPrintMethodDelegate = (Func<string, string>)dynamicCallPrintMethod.CreateDelegate(typeof(Func<string, string>));
             Assert.AreEqual(callPrintMethodDelegate("jim"), "the value passed to Print is jim");
+        }
+
+        [TestMethod]
+        public void Assert_Dynamic_Method_Is_called_By_Dynamic_Method()
+        {
+            var mult = new DynamicMethod(
+                                   "mult",
+                                   typeof(int),
+                                   new[] { typeof(int), typeof(int) },
+                                   typeof(MsilTest).Module);
+
+            var il = mult.GetILGenerator();
+            il.Emit(OpCodes.Ldarg_0);
+            il.Emit(OpCodes.Ldarg_1);
+            il.Emit(OpCodes.Mul);
+            il.Emit(OpCodes.Ret);
+
+            var calc = new DynamicMethod(
+                                   "calc",
+                                   typeof(int),
+                                   new[] { typeof(int), typeof(int) },
+                                   typeof(MsilTest).Module);
+
+            il = calc.GetILGenerator();
+            il.Emit(OpCodes.Ldarg_0);
+            il.Emit(OpCodes.Ldarg_1);
+            il.Emit(OpCodes.Call, mult);
+            il.Emit(OpCodes.Ret);
+
+            var calcDelegate = (Func<int, int, int>)calc.CreateDelegate(typeof(Func<int, int, int>));
+            Assert.AreEqual(calcDelegate(2, 3), 6);
+        }
+
+        [TestMethod]
+        public void Assert_Recursive_Dynamic_Method_Is_Created_By_Reflection()
+        {
+            var recursive = new DynamicMethod(
+                                     "recursive",
+                                     typeof(int),
+                                     new[] { typeof(int) },
+                                     typeof(MsilTest).Module);
+
+            var il = recursive.GetILGenerator();
+
+            var ret1Label = il.DefineLabel();
+
+            // If i == 1
+            il.Emit(OpCodes.Ldarg_0);
+            il.Emit(OpCodes.Ldc_I4_1);
+            il.Emit(OpCodes.Beq, ret1Label);
+
+            //load i 
+            il.Emit(OpCodes.Ldarg_0);
+            il.Emit(OpCodes.Ldc_I4_1);
+            il.Emit(OpCodes.Sub);
+            il.Emit(OpCodes.Call, recursive);
+            il.Emit(OpCodes.Ldarg_0);
+            il.Emit(OpCodes.Mul);
+            il.Emit(OpCodes.Ret);
+
+            // Return 1
+            il.MarkLabel(ret1Label);
+            il.Emit(OpCodes.Ldc_I4_1);
+            il.Emit(OpCodes.Ret);
+
+            var recursiveDelegate = (Func<int, int>)recursive.CreateDelegate(typeof(Func<int, int>));
+            Assert.AreEqual(recursiveDelegate(5), 120);
+        }
+
+        [TestMethod]
+        public void Assert_Dynamic_Method_That_Contains_Switch_Case_Is_Created_By_Reflection()
+        {
+            var switchCase = new DynamicMethod(
+                                        "switchCase",
+                                        typeof(int),
+                                        new[] { typeof(int), typeof(int), typeof(int) },
+                                        typeof(MsilTest).Module);
+
+            var il = switchCase.GetILGenerator();
+
+            var caseAdd = il.DefineLabel();
+            var caseMul = il.DefineLabel();
+            var caseDiv = il.DefineLabel();
+            var caseSub = il.DefineLabel();
+
+
+
+            il.Emit(OpCodes.Ldarg_2);
+            il.Emit(OpCodes.Ldc_I4_0);
+            il.Emit(OpCodes.Beq, caseAdd);
+
+            il.Emit(OpCodes.Ldarg_2);
+            il.Emit(OpCodes.Ldc_I4_1);
+            il.Emit(OpCodes.Beq, caseMul);
+
+            il.Emit(OpCodes.Ldarg_2);
+            il.Emit(OpCodes.Ldc_I4_2);
+            il.Emit(OpCodes.Beq, caseDiv);
+
+            il.Emit(OpCodes.Ldarg_2);
+            il.Emit(OpCodes.Ldc_I4_3);
+            il.Emit(OpCodes.Beq, caseSub);
+
+            //default
+            il.Emit(OpCodes.Ldc_I4_0);
+            il.Emit(OpCodes.Ret);
+
+            il.MarkLabel(caseAdd);
+            il.Emit(OpCodes.Ldarg_0);
+            il.Emit(OpCodes.Ldarg_1);
+            il.Emit(OpCodes.Add);
+            il.Emit(OpCodes.Ret);
+
+            il.MarkLabel(caseMul);
+            il.Emit(OpCodes.Ldarg_0);
+            il.Emit(OpCodes.Ldarg_1);
+            il.Emit(OpCodes.Mul);
+            il.Emit(OpCodes.Ret);
+
+            il.MarkLabel(caseDiv);
+            il.Emit(OpCodes.Ldarg_0);
+            il.Emit(OpCodes.Ldarg_1);
+            il.Emit(OpCodes.Div);
+            il.Emit(OpCodes.Ret);
+
+            il.MarkLabel(caseSub);
+            il.Emit(OpCodes.Ldarg_0);
+            il.Emit(OpCodes.Ldarg_1);
+            il.Emit(OpCodes.Sub);
+            il.Emit(OpCodes.Ret);
+
+            var switchCaseDelegate = (Func<int, int, int, int>)switchCase.CreateDelegate(typeof(Func<int, int, int, int>));
+
+            Assert.AreEqual(switchCaseDelegate(6, 3, 0), 9);
+            Assert.AreEqual(switchCaseDelegate(6, 3, 1), 18);
+            Assert.AreEqual(switchCaseDelegate(6, 3, 2), 2);
+            Assert.AreEqual(switchCaseDelegate(6, 3, 3), 3);
+
+        }
+
+        [TestMethod]
+        public void Assert_Type_Is_Created_By_Reflection()
+        {
+            //https://blogs.msdn.microsoft.com/junfeng/2005/02/12/netmodule-vs-assembly/
+            var assemblyBuilder = AppDomain.CurrentDomain.DefineDynamicAssembly(
+                new AssemblyName("Demo"),
+                AssemblyBuilderAccess.Run
+            );
+            var moduleBuilder = assemblyBuilder.DefineDynamicModule("PersonModule");
+            var typeBuilder = moduleBuilder.DefineType("Person", TypeAttributes.Public);
+            var nameField = typeBuilder.DefineField("name", typeof(string), FieldAttributes.Private);
+
+            var constructor = typeBuilder.DefineConstructor(
+                MethodAttributes.Public,
+                CallingConventions.Standard,
+                new[] { typeof(string) });
+            var constructorIL = constructor.GetILGenerator();
+            constructorIL.Emit(OpCodes.Ldarg_0);
+            constructorIL.Emit(OpCodes.Ldarg_1);
+            constructorIL.Emit(OpCodes.Stfld, nameField);
+            constructorIL.Emit(OpCodes.Ret);
+
+            var nameProperty = typeBuilder.DefineProperty("Name", PropertyAttributes.HasDefault, typeof(string), null);
+            var namePropertyGetMethod = typeBuilder.DefineMethod(
+                "get_Name",
+                MethodAttributes.Public |
+                MethodAttributes.SpecialName |
+                MethodAttributes.HideBySig
+                ,
+                typeof(string),
+                Type.EmptyTypes
+                );
+            nameProperty.SetGetMethod(namePropertyGetMethod);
+            var namePropertyGetMethodIL = namePropertyGetMethod.GetILGenerator();
+            namePropertyGetMethodIL.Emit(OpCodes.Ldarg_0);
+            namePropertyGetMethodIL.Emit(OpCodes.Ldfld, nameField);
+            namePropertyGetMethodIL.Emit(OpCodes.Ret);
+
+            var personType = typeBuilder.CreateType();
+            var nProperty = personType.GetProperty("Name");
+            var person = Activator.CreateInstance(personType, "Jim");
+            var nameResult = nProperty.GetValue(person, null);
+            Assert.AreEqual(nameResult,"Jim");
         }
     }
 }
